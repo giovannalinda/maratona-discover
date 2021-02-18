665fb45000191fe1e51083f215eb6315f247c54b
giovannalinda <eugiovannasouza@gmail.com>
2021-02-18 01:09:05 -0300

    finished dark mode

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
 const Modal = {
   open() {
-    // Abrir modal
     // Adicionar a class active ao modal
     document.querySelector('.modal-overlay').classList.add('active');
   },
   close() {
-    // fechar o modal
     // remover a class active do modal
     document.querySelector('.modal-overlay').classList.remove('active');
   },
